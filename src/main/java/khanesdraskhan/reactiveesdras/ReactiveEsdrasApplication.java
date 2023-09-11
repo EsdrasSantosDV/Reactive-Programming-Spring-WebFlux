d01c585884fb000514cfaadd64e83f369b50c966
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-09-11 19:45:24 -0300

    feat:initial repo

---
+++ src/main/java/khanesdraskhan/reactiveesdras/ReactiveEsdrasApplication.java
@@ -0,0 +1,13 @@
+package khanesdraskhan.reactiveesdras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveEsdrasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveEsdrasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/khanesdraskhan/reactiveesdras/ReactiveEsdrasApplicationTests.java
@@ -0,0 +1,13 @@
+package khanesdraskhan.reactiveesdras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveEsdrasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
